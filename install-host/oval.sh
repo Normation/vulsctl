0907bc5ed73b0a3c4e89bf29570f5e3dc4cea559
Vincent Membré <vincent.membre@rudder.io>
2025-08-12 16:21:44 +0200

    Update with latest OS (debian 13, rhel 10)

--- install-host/oval.sh
+++ install-host/oval.sh
@@ -11,13 +11,13 @@ shift
 # NOTE: fetches oval of the OS with security support enabled.
 case "$target" in
 	--redhat)
-		goval-dictionary fetch redhat ${@} 6 7 8 9
+		goval-dictionary fetch redhat ${@} 6 7 8 9 10
 		;;
 	--amazon)
 		goval-dictionary fetch amazon ${@} 2 2023
 		;;
 	--debian)
-		goval-dictionary fetch debian ${@} 10 11 12
+		goval-dictionary fetch debian ${@} 10 11 12 13
 		;;
 	--ubuntu)
 		goval-dictionary fetch ubuntu ${@} 16.04 18.04 20.04 22.04 24.04
@@ -26,7 +26,7 @@ case "$target" in
 		goval-dictionary fetch alpine ${@} 3.17 3.18 3.19 3.20
 		;;
 	--oracle)
-		goval-dictionary fetch oracle ${@} 6 7 8 9
+		goval-dictionary fetch oracle ${@} 6 7 8 9 10
 		;;
 	--fedora)
 		goval-dictionary fetch fedora ${@} 40 41
